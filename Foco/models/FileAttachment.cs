0147f707f8884cd6dc61fff84794ee8d56f71946
MaziarTiari <maziar.tiari@gmail.com>
2019-11-20 18:01:18 +0100

    Basisklassen erstellt-initial commit

---
+++ Foco/models/FileAttachment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace testFoco.models
+{
+    class FileAttachment : Attachment
+    {
+        private string path;
+        private string comment;
+        public FileAttachment(string title, string path) : base(path)
+        {
+            base.title = title;
+            if (string.IsNullOrWhiteSpace(path)) throw new System.ArgumentException("message", nameof(path));
+            this.Path = path;
+        }
+        public string Comment
+        {
+            get => comment;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(comment)) return;
+                comment = value;
+            }
+        }
+
+        public string Path { get => path; set => path = value; }
+
+        public void OpenFile(string filePath, string programPath)
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = programPath;
+            p.StartInfo.Arguments = filePath;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            //start the exe
+            p.Start();
+        }
+    }
+}
---
+++ Foco/models/Task.cs
@@ -0,0 +1,20 @@
+﻿namespace testFoco.models
+{
+    public class Task
+    {
+        private string title;
+        private string description;
+        private bool done = false;
+        private Attachment attachment;
+
+        public Task(string title)
+        {
+            this.Title = title;
+        }
+
+        public string Description { get => description; set => description = value; }
+        public bool Done { get => done; set => done = value; }
+        public string Title { get => title; set => title = value; }
+        Attachment Attachment { get => attachment; set => attachment = value; }
+    }
+}
