78acdae6932b1b2f4236cd5aae98f7ae170f91cb
MaziarTiari <maziar.tiari@gmail.com>
2019-11-20 18:45:14 +0100

    modified .gitignore, added new class: Comment, and added new attribute: comments to Attachment

--- Foco/models/Attachment.cs
+++ Foco/models/Attachment.cs
@@ -1,14 +1,23 @@
-﻿namespace testFoco.models
+﻿using System.Collections.Generic;
+
+namespace testFoco.models
 {
     public class Attachment
     {
         protected string title;
         protected string description;
+        private List<Comment> comments = new List<Comment>();
         public Attachment(string title)
         {
             this.Title = title;
         }
         public string Title { get => title; set => title = value; }
         public string Description { get => description; set => description = value; }
+        protected List<Comment> Comments => comments;
+
+        public void AddComment(string comment)
+        {
+            Comments.Add(new Comment("comment"));
+        }
     }
 }
