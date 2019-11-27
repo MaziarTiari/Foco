8a38eb91e9b8cb1f061e14943fae976937f1c636
kit0kat <nicohofmann.2000@gmail.com>
2019-11-27 18:40:41 +0100

    Confirm Window erstellt

---
+++ Foco/windows/ConfirmWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Foco.windows
+{
+
+    public enum ConfirmState { YES, NO };
+
+    /// <summary>
+    /// Interaktionslogik für ConfirmWindow.xaml
+    /// </summary>
+    public partial class ConfirmWindow : Window
+    {
+
+        public delegate void ConfirmCallback(ConfirmState confirmState);
+
+        private readonly ConfirmCallback confirmCallback;
+
+        public ConfirmWindow(string title, string message, ConfirmCallback confirmCallback)
+        {
+            InitializeComponent();
+            this.confirmCallback = confirmCallback;
+            Owner = Application.Current.MainWindow;
+            Title = title;
+            Message.Text = message;
+        }
+
+        // Benutzer drück "Ja" oder "Nein"
+        private void OnButtonClicked(object sender, RoutedEventArgs e)
+        {
+            Close();
+            switch (((Button)sender).Tag)
+            {
+                case "Yes": confirmCallback(ConfirmState.YES); break;
+                case "No": confirmCallback(ConfirmState.NO); break;
+            }
+        }
+
+    }
+}
