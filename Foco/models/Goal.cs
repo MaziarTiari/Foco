fe68ee03045960bdbc8cf4b0cab92fa2f89c3c7e
kit0kat <nicohofmann.2000@gmail.com>
2019-11-29 18:40:50 +0100

    Inkonsistenz bei Exceptions gefixt

--- Foco/models/Goal.cs
+++ Foco/models/Goal.cs
@@ -4,30 +4,29 @@ namespace Foco.models
 {
     public class Goal
     {
-        
+
         private string title;
-        private List<Project> projects = new List<Project>();
-        
+        private List<Project> projects;
+
         public Goal(string title)
         {
-            if (string.IsNullOrWhiteSpace(title))
-                throw new ArgumentNullException();
-            this.Title = title;
+            Projects = new List<Project>();
+            Title = title;
         }
-        
-        public string Title { get => title; set => title = value; }
-        public List<Project> Projects { get => projects; set => projects = value; }
-        
+
+        public string Title { get => title; set { if (string.IsNullOrWhiteSpace(value)) throw new ArgumentNullException(); title = value; } }
+        public List<Project> Projects { get => projects; set { if (value == null) throw new ArgumentNullException(); projects = value; } }
+
         // eigentlich unnötig, da man direkt auf die Liste zugreifen darf?
         public void AddProject(Project project)
         {
             if (projects.Exists(x => x.Name == project.Name)) throw new DuplicateWaitObjectException();
             else projects.Add(project);
         }
-        
+
         public Project GetProjectByName(string name)
         {
-            if(projects.Count == 0) return null;
+            if (projects.Count == 0) return null;
             if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("message", nameof(name));
             return projects.Find(x => x.Name == name);
         }
--- Foco/models/Taskgroup.cs
+++ Foco/models/Taskgroup.cs
@@ -9,27 +9,30 @@ namespace Foco.models
 
     public class Taskgroup
     {
-        
+
         private string title;
         private Priority prio;
-        private State state = State.Todo;
-        private List<Task> tasks = new List<Task>();
-        private List<Attachment> attachments = new List<Attachment>();
+        private State state;
+        private List<Task> tasks;
+        private List<Attachment> attachments;
         private DateTime deadline;
 
         public Taskgroup(string title)
         {
-            if (string.IsNullOrWhiteSpace(title))
-                throw new ArgumentNullException();
-            this.title = title;
+            Title = title;
+            State = State.Todo;
+            Prio = Priority.Mid;
+            Tasks = new List<Task>();
+            Attachments = new List<Attachment>();
+            Deadline = DateTime.MinValue; // bedeutet keine Deadline
         }
-        
+
         public Priority Prio { get => prio; set => prio = value; }
         public State State { get => state; set => state = value; }
-        public string Title { get => title; set => title = value; }
-        public DateTime Deadline { get => deadline; set => deadline = value; }
-        public List<Task> Tasks { get => tasks; set => tasks = value; }
-        public List<Attachment> Attachments { get => attachments; set => attachments = value; }
+        public string Title { get => title; set { if (string.IsNullOrWhiteSpace(value)) throw new ArgumentNullException(); title = value; } }
+        public DateTime Deadline { get => deadline; set { if (value == null) throw new ArgumentNullException(); deadline = value; } }
+        public List<Task> Tasks { get => tasks; set { if (value == null) throw new ArgumentNullException(); tasks = value; } }
+        public List<Attachment> Attachments { get => attachments; set { if (value == null) throw new ArgumentNullException(); attachments = value; } }
 
         public Task GetTaskByName(string name)
         {
