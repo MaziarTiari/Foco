4f48343b7603061a4ebc648d37777ecc1fb27949
kit0kat <nicohofmann.2000@gmail.com>
2019-12-03 17:36:26 +0100

    Edit und Delete Button nur bei Hover anzeigen

--- Foco/controls/TaskgroupControl.xaml.cs
+++ Foco/controls/TaskgroupControl.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace Foco.controls
@@ -18,6 +19,8 @@ namespace Foco.controls
 
         public TaskgroupControl(Taskgroup taskgroup)
         {
+            if (taskgroup == null)
+                throw new ArgumentNullException();
             InitializeComponent();
             this.taskgroup = taskgroup;
             Update();
@@ -25,41 +28,54 @@ namespace Foco.controls
 
         private void Update()
         {
-            if (taskgroup != null)
+            DeleteButton.Visibility = Visibility.Hidden;
+            EditButton.Visibility = Visibility.Hidden;
+            NameLabel.Content = taskgroup.Title;
+            switch (taskgroup.Prio)
             {
-                NameLabel.Content = taskgroup.Title;
-                switch (taskgroup.Prio)
-                {
-                    case Priority.High:
-                        PriorityBorder.Background = new SolidColorBrush(Colors.Red);
-                        PriorityLabel.Content = "Hoch";
-                        break;
-                    case Priority.Mid:
-                        PriorityBorder.Background = new SolidColorBrush(Colors.Orange);
-                        PriorityLabel.Content = "Normal";
-                        break;
-                    case Priority.Low:
-                        PriorityBorder.Background = new SolidColorBrush(Colors.Green);
-                        PriorityLabel.Content = "Niedrig";
-                        break;
-                }
-                if (taskgroup.Deadline == DateTime.MinValue)
-                {
-                    DeadlineBorder.Visibility = Visibility.Hidden;
-                }
-                else
-                {
-                    DeadlineBorder.Visibility = Visibility.Visible;
-                    DeadlineLabel.Content = taskgroup.Deadline.ToString("dd.MM.yyyy HH:mm");
-                }
-                int countAll = 0, countDone = 0;
-                foreach (Task task in taskgroup.Tasks)
-                {
-                    countAll++;
-                    countDone += task.Done ? 1 : 0;
-                }
-                TasksLabel.Content = countDone + " / " + countAll + " Aufgaben";
+                case Priority.High:
+                    PriorityBorder.Background = new SolidColorBrush(Colors.Red);
+                    PriorityLabel.Content = "Hoch";
+                    break;
+                case Priority.Mid:
+                    PriorityBorder.Background = new SolidColorBrush(Colors.Orange);
+                    PriorityLabel.Content = "Normal";
+                    break;
+                case Priority.Low:
+                    PriorityBorder.Background = new SolidColorBrush(Colors.Green);
+                    PriorityLabel.Content = "Niedrig";
+                    break;
             }
+            if (taskgroup.Deadline == DateTime.MinValue)
+            {
+                DeadlineBorder.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                DeadlineBorder.Visibility = Visibility.Visible;
+                DeadlineLabel.Content = taskgroup.Deadline.ToString("dd.MM.yyyy HH:mm");
+            }
+            int countAll = 0, countDone = 0;
+            foreach (Task task in taskgroup.Tasks)
+            {
+                countAll++;
+                countDone += task.Done ? 1 : 0;
+            }
+            TasksLabel.Content = countDone + " / " + countAll + " Aufgaben";
+        }
+
+        // Benutzer beginnt Hover
+        private void OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            DeleteButton.Visibility = Visibility.Visible;
+            EditButton.Visibility = Visibility.Visible;
+        }
+
+        // Benutzer endet Hover
+        private void OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            DeleteButton.Visibility = Visibility.Hidden;
+            EditButton.Visibility = Visibility.Hidden;
         }
 
     }
