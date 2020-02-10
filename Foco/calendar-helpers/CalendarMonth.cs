69deb2a1869fc00f4fa4cffcecd317cdcd352fdf
MaziarTiari <maziar.tiari@gmail.com>
2020-02-10 02:44:12 +0100

    refactor to 90 character per line

--- Foco/calendar-helpers/CalendarMonth.cs
+++ Foco/calendar-helpers/CalendarMonth.cs
@@ -7,12 +7,13 @@ namespace Foco.calendar
     public class CalendarMonth
     {
         public static readonly string[] MonthNames = new string[] {
-            "Jan", "Feb", "März", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"
+            "Jan", "Feb", "März", "Apr", "Mai", "Jun", 
+            "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"
         };
 
         private int month;
         private int year;
-        private int weekdayIndexOfMonthsFirstDay;
+        private int monthsFirstWeekdayIndex;
         private CalendarDay[] days = new CalendarDay[42];
         private List<Taskgroup> taskgroups = new List<Taskgroup>();
         public CalendarMonth(int year, int month)
@@ -22,18 +23,30 @@ namespace Foco.calendar
             Update();
         }
 
-        public int Month { get => month; set { month = value; Update(); } }
-        public int Year { get => year; set { year = value; Update(); } }
-        public List<Taskgroup> Taskgroups { get => taskgroups; set { taskgroups = value; Update(); } }
+        public int Month 
+        { 
+            get => month; 
+            set { month = value; Update(); } 
+        }
+        public int Year 
+        { 
+            get => year; 
+            set { year = value; Update(); } 
+        }
+        public List<Taskgroup> Taskgroups 
+        { 
+            get => taskgroups; 
+            set { taskgroups = value; Update(); } 
+        }
         public CalendarDay[] Days { get => days; set => days = value; }
 
-        public int PreviousMonth() 
+        private int PreviousMonth() 
         {
             if (Month == 1) return 12;
             else return Month - 1;
         }
 
-        public int NextMonth()
+        private int NextMonth()
         {
             if (Month == 12) return 1;
             else return Month + 1;
@@ -42,18 +55,20 @@ namespace Foco.calendar
         private void Update()
         {
             DateTime startDateOfMonth = new DateTime(Year, Month, 1);
-            weekdayIndexOfMonthsFirstDay = startDateOfMonth.DayOfWeek.GetHashCode();
+            monthsFirstWeekdayIndex = startDateOfMonth.DayOfWeek.GetHashCode();
             int numberOfDaysInSelectedMonth = DateTime.DaysInMonth(Year, Month);
-            int numberOfDaysInPreviousMonth = DateTime.DaysInMonth(YearOfPreviousMonth(), PreviousMonth());
+            int numberOfDaysInPreviousMonth = 
+                DateTime.DaysInMonth(YearOfPreviousMonth(), PreviousMonth());
 
             int startingIndex, endingIndex, startingDay;
             // Add days from previous month
             startingIndex = 0;
-            endingIndex = weekdayIndexOfMonthsFirstDay;
-            startingDay = numberOfDaysInPreviousMonth - weekdayIndexOfMonthsFirstDay + 1;
+            endingIndex = monthsFirstWeekdayIndex;
+            startingDay = numberOfDaysInPreviousMonth - monthsFirstWeekdayIndex + 1;
             AddDaysOfMonth(
-                startingIndex, endingIndex, YearOfPreviousMonth(), PreviousMonth(), startingDay, false
-                );
+                startingIndex, endingIndex, YearOfPreviousMonth(), PreviousMonth(), 
+                startingDay, false);
+
             // Add days from selected month
             startingIndex = endingIndex;
             endingIndex = numberOfDaysInSelectedMonth + endingIndex;
@@ -62,17 +77,18 @@ namespace Foco.calendar
             // Add days from next month
             startingIndex = endingIndex;
             endingIndex = Days.Length;
-            AddDaysOfMonth(startingIndex, endingIndex, YearOfNextMonth(), NextMonth(), 1, false);
+            AddDaysOfMonth(
+                startingIndex, endingIndex, YearOfNextMonth(), NextMonth(), 1, false);
         }
 
-
-        private void AddDaysOfMonth(int index, int max , int y, int m, int startingDay, bool areFromSelectedMonth)
+        private void AddDaysOfMonth(int index, int max , int y, int m, 
+                                    int startingDay, bool fromSelectedMonth)
         {
             for (int i = index; i < max; i++)
             {
                 AddDay(i, new DateTime(y, m, startingDay));
                 startingDay++;
-                Days[i].FromSelectedMonth = areFromSelectedMonth;
+                Days[i].FromSelectedMonth = fromSelectedMonth;
             }
         }
 
